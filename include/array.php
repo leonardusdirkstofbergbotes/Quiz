0309083827a513561ec7cc37a5e54070f0f5965d
Leonardus <leonardusdirkstofbergbotes@gmail.com>
2020-03-06 08:11:13 +0200

    made questions array in separated file and folder
    Will include in my quiz page

---
+++ include/array.php
@@ -0,0 +1,124 @@
+<?php
+        $master = array('1 How do you start a proper html document between < >'
+                                => array( 
+                                    'html' => 'a',
+                                    '!DOCTYPE html' => 'd',
+                                    '/html' => 'c',
+                                    'DOCTYPE = html' => 'b'),
+                            '2 Does all html tags have a closing tag'
+                                => array(
+                                    'Not all of them' => 'd',
+                                    'Yes' => 'c',
+                                    'Never' => 'a',
+                                    'Always' => 'b'),
+                            '3 How many heading tags are there' 
+                                => array(
+                                    'One' => 'c',
+                                    'Three' => 'a',
+                                    'Six' => 'd',
+                                    'As many as you like' => 'b'),
+                            '4 Whats the proper syntax for comments in html?' 
+                                => array(
+                                    '--comment--' => 'a',
+                                    '!--comment--' => 'd',
+                                    '//comment//' => 'c',
+                                    '/*comment*' => 'b'),
+                            '5 What will happen if I insert a br element?' 
+                                => array(
+                                    'Nothing' => 'b',
+                                    'It will bold the text' => 'c',
+                                    'It will execute a full stop' => 'a',
+                                    'A line break will be executed' => 'd'),
+                            '6 If a browser cant find an image inside a img tag it will resort to what?' 
+                                => array(
+                                    'It will skip the img tag' => 'c',
+                                    'An error message will pop up' => 'b',
+                                    'It will display whatever is in the alt tag' => 'd',
+                                    'The computer will freeze' => 'a'),
+                            '7 A th tag represent the?' 
+                                => array(
+                                    'The table header' => 'd',
+                                    'A table height' => 'a',
+                                    'A table href' => 'b',
+                                    'There is no such tag' => 'c'),
+                            '8 What do we use an iframe tag for?'
+                                => array(
+                                    'To display a webpage within another webpage' => 'd',
+                                    'A clickable link that will take you take you to that webpage' => 'a',
+                                    'A box with a border around it' => 'b',
+                                    'A section that contains information' => 'c'),
+                            '9 The style element is used to do inline styling, But where do you place the style element?' 
+                                => array(
+                                    'In an external css file' => 'b',
+                                    'You dont need a style tag to style an element' => 'c',
+                                    'Next to the element that you want to style' => 'a',
+                                    'Just before you close the <head> tag' => 'd'),
+                            '10 How does one write a single-line comment in CSS?' 
+                                => array(
+                                    '/*comment*' => 'c',
+                                    '!--comment--' => 'b',
+                                    '//comment//' => 'd',
+                                    '--comment--' => 'a'),
+                            '11 Which one is the correct CSS syntax'
+                                => array( 
+                                    'body: property = value' => 'a',
+                                    'body {property: value; }' => 'd',
+                                    'body style=value' => 'c',
+                                    'body {value = property;' => 'b'),
+                            '12 How do you target a class in CSS'
+                                => array(
+                                    '.class {}' => 'd',
+                                    '#class {}' => 'c',
+                                    'class{}' => 'a',
+                                    'element_class {}' => 'b'),
+                            '13 Which one of these refers to an id in CSS' 
+                                => array(
+                                    'id.name{}' => 'c',
+                                    '.name{}' => 'a',
+                                    '#name {}' => 'd',
+                                    'name=id {}' => 'b'),
+                            '14 Which property changes the color of text in css?' 
+                                => array(
+                                    'font-color' => 'a',
+                                    'color' => 'd',
+                                    'text-color' => 'c',
+                                    'bgcolor' => 'b'),
+                            '15 Which property is used to change the font of text?' 
+                                => array(
+                                    'font-decoration' => 'b',
+                                    'text-font' => 'c',
+                                    'font' => 'a',
+                                    'font-family' => 'd'),
+                            '16 How would you change the margin on the right to 10px?' 
+                                => array(
+                                    'padding-left: 10px;' => 'c',
+                                    'margin: 10px right;' => 'b',
+                                    'margin-right: 10px;' => 'd',
+                                    'margin: 10px;' => 'a'),
+                            '17 How do we group selectors in CSS?' 
+                                => array(
+                                    'p, h1, em {}' => 'd',
+                                    'selectors cannot be grouped' => 'a',
+                                    'p. h1. em {}' => 'b',
+                                    'p + h1 + em {}' => 'c'),
+                            '18 What is the default value of position?'
+                                => array(
+                                    'static' => 'd',
+                                    'block' => 'a',
+                                    'relative' => 'b',
+                                    'there is no default' => 'c'),
+                            '19 Are all the CSS property fully supported in all browsers?' 
+                                => array(
+                                    'if your graphics card allows it' => 'b',
+                                    'Depends on the time of day' => 'c',
+                                    'Yes' => 'a',
+                                    'No' => 'd'),
+                            '20 What does CSS short for?' 
+                                => array(
+                                    'Creative Style Sheets' => 'c',
+                                    'Cascading Sheet Styles' => 'b',
+                                    'Cascading Style Sheets' => 'd',
+                                    'Creative Sheet Styles' => 'a')    
+                                );
+
+    ?>
