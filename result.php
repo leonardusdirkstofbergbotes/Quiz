21340ecbcee65a081fd9bbb1c2e0a2bf4406b7e8
Leonardus <leonardusdirkstofbergbotes@gmail.com>
2020-03-06 13:08:54 +0200

    created the result page that will calculate the score

---
+++ result.php
@@ -0,0 +1,17 @@
+<?php 
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Results</title>
+    <link rel="stylesheet" href="css/styles01.css">
+</head>
+<body>
+
+
+</body>
+</html>
