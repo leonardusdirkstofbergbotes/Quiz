5af8e7ef27c3c3ed6b51b8f9842538a7dbe5a6ab
Leonardus <leonardusdirkstofbergbotes@gmail.com>
2020-03-07 10:57:17 +0200

    Added the page contents displaying your scored coupled with animations

--- result.php
+++ result.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Results</title>
-    <link rel="stylesheet" href="css/styles01.css">
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
 <?php 
@@ -19,6 +19,11 @@
         }
     };
 ?>
+
+<fieldset class="border box mar_pad no_pad back_color animate_block" id="result">
+<p class="mar_pad font_larger white growing"><br><br> Well done <?php echo $_SESSION['username'] ?> You scored <br><br> <?php echo $score ?> <br>--<br>20</p>
+        <img class="top down" src="images/confetti.png">
+</fieldset>
 
 
 </body>
